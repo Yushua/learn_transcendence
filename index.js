1309026b3115521f2fe2fab923af0e914e08a174
Yushua <yushabakker1@gmail.com>
2022-11-30 16:54:11 +0100

    adding tutorial

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log('come on WORLD');
+
+//declaring information
+let name = 'yusha';
+console.log(name);
+
+let person = {
+    me: 'hello',
+    age: 40
+};
+
+console.log(person.age);
+//no reserved
+
+
+let selectedColours = ['red', 'blue'];
+selectedColours[10] = 4;
+console.log(selectedColours);
+
+function greet(name){
+    console.log(name);
+}
+
+greet('insert something');

d5e599d2e71af78c16adae353b466e3a65a71f0b
Yushua <yushabakker1@gmail.com>
2022-12-01 17:16:13 +0100

    adding new stuff for now

---
+++ index.js
@@ -0,0 +1,1 @@
+console.log('COME ON WORK IT');
---
+++ js_basics _2/index.js
@@ -0,0 +1,37 @@
+
+function createAccount(){
+    return {
+        //properties
+        name: "",
+        password: "",
+        timeOfCreation: ""
+    };
+    
+}
+
+function draw(input){
+    console.log(input);
+}
+
+let yusha = createAccount();
+yusha.name = "yusha";
+yusha.password = "Admin123";
+yusha.timeOfCreation = 0;
+
+draw(yusha.name);
+console.log(yusha.password);
+
+//constructor
+function CreateAccount(){
+    private:
+        this.name = "flipy";
+        this.password = "123Password";
+}
+
+let flipy = new CreateAccount();
+// this.flipy.name = "flipy";
+// flipy.name = "flipy";
+// flipy.password = "Admin123";
+
+draw(flipy.name);
+// console.log(yusha.password);

c0ad42ef5f1831d9117168dc5c1f385502ebdcf7
Yushua <yushabakker1@gmail.com>
2022-12-20 16:40:15 +0100

    aadd

--- nestjs_project/yusha_v1/src/app.controller.ts
+++ nestjs_project/yusha_v1/src/app.controller.ts
@@ -1,12 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+//creates a new URL
+@Controller("hi")
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  //it adds to the first one made
+  constructor(private readonly appService2: AppService) {}
+
+  @Get()
+  //send an hello
+  getHello(): string {
+    return this.appService2.getHello();
+  }
+}
+
+@Controller("hey")
+export class AppController2 {
+  //it adds to the first one made
+  constructor(private readonly appService2: AppService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService2.getHello();
   }
 }
